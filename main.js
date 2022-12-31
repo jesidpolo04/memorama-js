46d0a71a16b690f74509d2067448b1364b1644c9
jesidpolo04 <jesing482@gmail.com>
2022-12-31 13:11:26 -0500

    making the game mechanics (flipped cards)

--- main.js
+++ main.js
@@ -2,30 +2,71 @@
 
 document.addEventListener('click', (e)=>{
     if(e.target.classList.contains('card')){
-        flipCard(e.target)
+        play(e.target)
     }
 })
 
 /* Game State */
-cardsFlipped = 0;
-flippedCards = []
+const maxFlippableCards = 2
+let flippedCards = 0;
+let outPlayCards = []
+let cardsInPlay = []
 
 /* Player actions */
-function flipCard(card){
+function play(card){
     if(!isFlippedCard(card)){
-        if(cardsFlipped >= 2){
-            throw Error("Player Can´t flip more than two cards per turn")
+        if( !(flippedCards < maxFlippableCards) ){
+            throw Error("Player Can't flip more than two cards per turn")
         }
-        card.classList.add('flipped');
-        cardsFlipped ++;
-    }  
+        flipCard(card)
+        flippedCards ++;
+        cardsInPlay.push(card)
+        console.info('flipped cards', cardsInPlay)
+        if(flippedCards >= maxFlippableCards){
+            finishTurn()
+        }
+    }
 }
 
+function flipCard(card){
+    if(!isFlippedCard(card)){
+        card.classList.add('flipped');
+    }else{
+        card.classList.remove('flipped')
+    }
+}
+
+/**
+ * @returns {boolean}
+ */
 function isFlippedCard(card){
     return card.classList.contains('flipped')
 }
 
 /* Game actions */
 function finishTurn(){
-    
+    if( cardMatch(cardsInPlay[0], cardsInPlay[1]) ){
+        outPlayCards.push(...cardsInPlay)
+    }else{
+        flipCards(cardsInPlay)
+    }
+    cardsInPlay = [];
+    flippedCards = 0;
+}
+
+function flipCards(cards){
+    console.log('flip cards')
+    cards.forEach(card => {
+        flipCard(card)
+    });
+}
+
+/**
+ * @param {HTMLElement} cardOne
+ * @param {HTMLElement} cardTwo
+ * @returns {boolean}
+ */
+function cardMatch(cardOne, cardTwo){
+    console.log(cardOne.dataset.code === cardTwo.dataset.code ? true : false)
+    return cardOne.dataset.code === cardTwo.dataset.code ? true : false
 }
